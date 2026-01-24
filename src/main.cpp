aa201e0e1865c8a68dadd353a712a7c9a8032a6d
AndrewJMart <amart89531@gmail.com>
2026-01-24 19:17:15 +0000

    Edit: Basic Crow APP

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
+#include <crow.h>
 using namespace std;
 
 int main(){
     cout << "Hello Backend!";
-    return 0;
+
+    crow::SimpleApp app; //define your crow application
+
+    //define your endpoint at the root directory
+    CROW_ROUTE(app, "/")([](){
+        return "Hello world";
+    });
+
+    //set the port, set the app to run on multiple threads, and run the app
+    app.port(18080).multithreaded().run();
 }
+
