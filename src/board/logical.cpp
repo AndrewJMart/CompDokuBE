4a7dd5ac536a0e5ffb3f929f7783604fb4dcfa6d
AndrewJMart <amart89531@gmail.com>
2026-01-29 07:33:59 +0000

    Add: More logic to createCandidateSet

--- src/board/logical.cpp
+++ src/board/logical.cpp
@@ -19,5 +19,42 @@ bool Board::logicalSolver(std::vector<std::vector<int>> board){
 }
 
 void createCandidateSet(board, candidate_set) {
+    // Given A Board, Create a Candidate Set
+
+    // Create Masks For Rows, Cols, Matrices (9x9 Board Assumption)
+    std::vector<int> col_mask(cols);
+    std::vector<int> row_mask(rows);
+    std::vector<int> matrix_mask(rows);
+
+    // Update masks
+    for (int row = 0; row < rows; row++) {
+        for (int col = 0; col < cols; col++) {
+            if (board[row][col] == 0) {
+                continue;
+            }
+
+            // Represent Number In Bit Format
+            int pos = 1 << (board[row][col] - 1);
+
+            // Bitwise Or To Set Bit
+            col_mask[col] |= pos;
+            row_mask[row] |= pos;
+            
+            // Calculate Matrix Position
+            int row_location = row / 3;
+            int col_location = col / 3;
+            int matrix_value = (row_location * 3) + col_location;
+
+            matrix_mask[matrix_value] |= pos;
+        }
+
+    // Iterate Through List & Update Candidate Set
+
+
+    }
+
+    // Iterate Through Boa
+
+
     return;
 }
