3008120dec3bf4c121d13cd37e42d6ca59d17405
AndrewJMart <amart89531@gmail.com>
2026-01-30 06:17:32 +0000

    Add: Naked Singles

--- src/board/logical.cpp
+++ src/board/logical.cpp
@@ -1,6 +1,23 @@
 #include "board.h"
 
 bool nakedSingles(std::vector<std::vector<int>>& board, std::vector<std::vector<int>>& candidate_set) {
-    return true;
-} 
+    // Naked Singles:
+    // Apply When Cell Only Has One Candidate Left! Simply Place Single Candidate Into Board
+    
+    bool progress = false;
 
+    for (int row = 0; row < rows; row++) {
+        for (int col = 0; col < cols; col++) {
+            if (board[row][col] != 0)
+                continue;
+
+            // Check If Unsolved Cell Has Only One Candidate
+            if ((candidate_set[row][col] & (candidate_set[row][col] - 1)) == 0) {
+                board[row][col] = candidate_set[row][col];
+                progress = true;
+            }
+        }
+    }
+
+    return progress;
+}
