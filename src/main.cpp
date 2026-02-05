d723f3fde37c97ff6ba5d3b2d72806418a497f15
AndrewJMart <amart89531@gmail.com>
2026-02-05 06:44:46 +0000

    Add: Basic Websocket Snippet

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <fstream>
-#include <nlohmann/json.hpp>
 #include <crow.h>
+#include <unordered_set>
+#include <mutex>
 
 // Import Sudoku Board Class
 #include "Board/Board.h"
@@ -15,6 +16,28 @@ int main(){
     std::cout << "Reading Backend Config";
 
     crow::SimpleApp app;
+    std::mutex mtx;
+    std::unordered_set<crow::websocket::connection*> users;
+
+    CROW_WEBSOCKET_ROUTE(app, "/ws")
+      .onopen([&](crow::websocket::connection& conn) {
+          CROW_LOG_INFO << "new websocket connection from " << conn.get_remote_ip();
+          std::lock_guard<std::mutex> _(mtx);
+          users.insert(&conn);
+      })
+      .onclose([&](crow::websocket::connection& conn, const std::string& reason, uint16_t) {
+          CROW_LOG_INFO << "websocket connection closed: " << reason;
+          std::lock_guard<std::mutex> _(mtx);
+          users.erase(&conn);
+      })
+      .onmessage([&](crow::websocket::connection& /*conn*/, const std::string& data, bool is_binary) {
+          std::lock_guard<std::mutex> _(mtx);
+          for (auto u : users)
+              if (is_binary)
+                  u->send_binary(data);
+              else
+                  u->send_text(data);
+      });
 
     // Default Endpoint
     CROW_ROUTE(app, "/")([](){
@@ -34,7 +57,7 @@ int main(){
 
         return returnBoard;
     });
-
+    
     // Single Thread App
     app.port(18080).multithreaded().run();
 }
